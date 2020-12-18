9b97a1fbf9bc0ad428df06d775908ae66484ca81
nazarenocamia <n.camia@campus.unimib.it>
2020-12-18 13:50:03 +0100

    fifth day - begin

--- lib/pictures.dart
+++ lib/pictures.dart
@@ -1,103 +1,154 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-import 'global.dart';
-
-
 class imagePick extends StatefulWidget {
   @override
   imagePick_State createState() => imagePick_State();
+  var _image;
+  var url;
 }
 
 class imagePick_State extends State<imagePick> {
-  File _image;
-  firebase_storage.FirebaseStorage storage;
-
-  @override
-  void initState() {
-    storage = firebase_storage.FirebaseStorage.instance;
-
-    super.initState();
-  }
-
-  Future getImage({ImageSource source}) async {
-    var pickedFile = await ImagePicker().getImage(source: source);
-    final File file = File(pickedFile.path);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-
-    await upload(file);
-  }
+  final Future<FirebaseApp> _inizialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Example'),
-      ),
-      body: Column(children: [
-        Center(
-          child:
-              _image == null ? Text('No image selected.') : Image.file(_image),
-        ),
-        custom_buttons(),
-        MaterialButton(
-            color: Colors.blue,
-            child: Text("UPLOAD"),
-            onPressed: () => Navigator.pop(context)),
-      ]),
-    );
+    return FutureBuilder(
+        future: _inizialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return Text(snapshot.error);
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(body: articleData());
+          }
+          return Text("Something bad happened...");
+        });
+    // return articleData();
   }
 
-  Widget custom_buttons() {
-    return Row(
+  Widget articleData() {
+    return Column(
       children: [
-        MaterialButton(
-          onPressed: () {
-            getImage(source: ImageSource.camera);
-          },
-          child: Icon(Icons.add_a_photo),
+        SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Center(
+              child: widget._image == null
+                  ? Text("no image selected")
+                  : Image.file(widget._image, fit: BoxFit.cover),
+            )),
+        RaisedButton(
+          color: Colors.blue,
+          child: Text("add image"),
+          onPressed: getImage,
         ),
-        MaterialButton(
-          onPressed: () {
-            getImage(source: ImageSource.gallery);
-          },
-          child: Icon(Icons.photo_size_select_actual_outlined),
-        ),
+        RaisedButton(
+          color: widget._image == null
+              ? CupertinoColors.inactiveGray
+              : Colors.blue,
+          child: Text("Upload Article"),
+          onPressed:()=> Navigator.pop(context,widget.url),
+        )
       ],
     );
   }
 
-  Future<void> upload(File image) async {
-    print("Starting upload...");
+  Future<void> getImage() async {
+    var pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+    final File file = File(pickedFile.path);
+    await uploadOnFirebaseAndGetLink(file);
+    setState(() {
+      if (pickedFile != null) {
+        widget._image = file;
+      } else {
+        print("No image selected");
+      }
+    });
+  }
+
+  Future<void> uploadOnFirebaseAndGetLink(File image) async {
     String filename = DateTime.now().toString();
+    var storage = await firebase_storage.FirebaseStorage.instance;
+
     var uploadData = await firebase_storage.FirebaseStorage.instance
         .ref()
-        .child("image/" + filename)
+        .child("BootcampImage/" + filename)
         .putFile(image);
-    print("uploadData: $uploadData");
     print(uploadData.runtimeType);
 
     String url = await firebase_storage.FirebaseStorage.instance
         .ref()
-        .child("image/" + filename)
+        .child("BootcampImage/" + filename)
         .getDownloadURL();
-
-    imgLink = url;
-
-    print(imgLink);
-
-    print("finished upload...");
-    return true;
+    widget.url = url;
+    print("the link is: $url");
   }
 }
+/*
+class articleCreationPage extends StatefulWidget {
+  @override
+  articleCreationPageState createState() => articleCreationPageState();
+}
+
+class articleCreationPageState extends State<articleCreationPage> {
+  String name;
+  int price;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 600,
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              showForm(),
+              imagePick(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showForm() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          articleName(),
+          articlePrice(),
+        ]);
+  }
+
+  Widget articleName() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(70, 70, 70, 0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.text,
+        decoration: new InputDecoration(labelText: "Item name"),
+        onSaved: (value) => name = value.trim(),
+      ),
+    );
+  }
+
+  Widget articlePrice() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(70, 30, 70, 0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.number,
+        decoration: new InputDecoration(labelText: "Item name"),
+        onSaved: (value) => price = value as int,
+      ),
+    );
+  }
+}*/
---
+++ lib/rounded_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class RoundedInput extends StatefulWidget {
+  final String hintText;
+  final Function(String) onChanged;
+  final String initialValue;
+
+  const RoundedInput({Key key, this.hintText, this.onChanged, this.initialValue}) : super(key: key);
+
+  @override
+  _RoundedInputState createState() => _RoundedInputState();
+}
+
+class _RoundedInputState extends State<RoundedInput> {
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _controller.text = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      child: TextField(
+        autofocus: false,
+        controller: _controller,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          //hintStyle: GoogleFonts.arimo(color: Colors.grey),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
